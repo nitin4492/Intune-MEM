fc6b45679aeca36bbc8be08259caa131fcb48430
Nitin Bhatt <75358886+nitin4492@users.noreply.github.com>
2022-03-14 08:05:56 +0100

    Add files via upload

---
+++ Find_DiscoveredApps_IntuneInventory.ps1
@@ -0,0 +1,26 @@
+﻿<#
+.SYNOPSIS
+	This script finds out the Discovered Apps in Intune
+
+.EXAMPLE
+#>
+
+param(
+[parameter(Mandatory=$True)]
+$AppName
+)
+
+Connect-MSGraph
+
+#Find Discovered Apps From Intune (It uses Intune Inventory to get apps from Add\RTemove Programs Entry)
+$FindApp=Get-IntuneDetectedApp -Filter "displayName eq '$AppName'"
+if($FindApp)
+{
+$result=$FindApp | Out-GridView -OutputMode:Single -Title "Select your Desired Discovered Application"
+Get-IntuneDetectedAppDevice -detectedAppId $result.id | Out-GridView
+}
+else
+{
+write-host "Particular App Not Discovered" -ForegroundColor Green
+}
+
